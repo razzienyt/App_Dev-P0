2ed9344035b7e68314ce4359413e3ae99836b7ef
Gryzza Langomes <gryzzalangomes44@gmail.com>
2025-02-09 16:17:41 +0800

    initial

---
+++ Phonebook.java
@@ -0,0 +1,173 @@
+public class Phonebook
+{
+    // Storage of contacts.
+    private Person[] contacts;
+    // Number of contacts present in the phonebook.
+    private int size;
+
+    /**
+     * Create a phonebook of size 50.
+     */
+    public Phonebook()
+    {
+        contacts = new Person[50];
+    }
+
+    /**
+     * @return Number of contacts stored in this phonebook.
+     */
+    public int getSize()
+    {
+        // Complete this method
+        return 0;
+    }
+
+    /**
+     * Get the contact at index.
+     * 
+     * @param index Index to get contact.
+     * @return Person object from index. Null if index is not valid or out of range.
+     */
+    public Person getContactAtIndex(int index)
+    {
+        if (index >= 0 && index < size) {
+            return contacts[index];
+        }
+        return null;
+    }
+
+    public Person getContact(String id)
+    {
+        for (int i = 0; i < size; i++) {
+            if (contacts[i].getId().equals(id)) {
+                return contacts[i];
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Checks if this phonebook has contacts or not.
+     * 
+     * @return True or False.
+     */
+    public boolean isEmpty()
+    {
+        return this.getSize() == 0;
+    }
+
+    /**
+     * Increase number of contacts present in this phonebook.
+     */
+    public void incrSize()
+    {
+        this.size++;
+    }
+
+    /**
+     * Decrease number of contacts present in this phonebook.
+     */
+    public void decrSize()
+    {
+        this.size--;
+    }
+
+    /**
+     * Increases the size of the phonebook whenever it is full.
+     */
+    private void increasePhonebookMaxSize()
+    {
+        Person[] newContacts = new Person[contacts.length * 2];
+        System.arraycopy(contacts, 0, newContacts, 0, contacts.length);
+        contacts = newContacts;
+    }
+    /**
+     * Inserts a new person object at its appropriate lexicographic location in the phonebook.
+     * 
+     * @param p Person to be addded to the Phonebook.
+     */
+    public void insert(Person p)
+    {
+        // Complete this method
+    }
+
+    /**
+     * Searches in what index should this person object with the given be inserted.
+     * 
+     * @param p Person object to be inserted into the phonebook.
+     * @return Appropriate index (position).
+     */
+    private int findIndexInsertion(Person p)
+    {
+        // Complete this method
+        return 0;
+    }
+
+    /**
+     * Delete a person based on their contact id.
+     * 
+     * @param id Contact ID of that contact.
+     * @return Deleted contact.
+     */
+    public Person deleteContact(String id)
+    {
+        // Complete this method...
+        return null;
+    }
+
+    /**
+     * Adjusts the existing contacts in a phonebook from a given starting index to where it ends,
+     * following a particular direction.
+     * 
+     * @param start Index to start adjustment from.
+     * @param end Index to end adjustment into.
+     * @param direction Direction in which the adjustment must be made. direction = "f" if element
+     *        at index 0 takes the value of the element next to it (e.g. index 1). direction = "b"
+     *        if element at index 1 takes the value of the element behind it (e.g. index 0).
+     */
+    
+    private void adjustPhonebook(int start, int end, String direction)
+     {
+         if (direction.equals("f")) {
+             for (int i = start; i < end; i++) {
+                 contacts[i] = contacts[i + 1];
+             }
+             contacts[end] = null; // Clear last entry after shift
+         } else if (direction.equals("b")) {
+             for (int i = end; i > start; i--) {
+                 contacts[i] = contacts[i - 1];
+             }
+             contacts[start] = null; // Clear first entry after shift
+         }
+     }
+
+    /**
+     * Uses ellipsis to ambiguously accept as many country codes as possible. <br>
+     * <br>
+     * For example: <br>
+     * <br>
+     * If we have: printContactsFromCountryCodes(1, 2, 3) <br>
+     * <br>
+     * Then we get: countryCodes = { 1, 2, 3 };
+     * 
+     * @param countryCodes Area codes to be used as a filter.
+     * @return Contacts on this phonebook under a particular area code set by the user.
+     */
+    public String printContactsFromCountryCodes(int... countryCodes)
+    {
+        // Complete this method.
+        return "";
+    }
+
+    /**
+     * Print the entire phonebook without any filter or so...
+     * 
+     * @return The entire list of contacts present in this phonebook.
+     */
+    public String toString()
+    {
+        // Complete this method.
+        return "";
+    }
+}

9eb3b8f0927f3c6940a369d50ba9bcda031c4727
razzienyt <timkamotycena@gmail.com>
2025-02-15 20:05:47 +0800

    kani raw

--- Person.java
+++ Person.java
@@ -1,12 +1,10 @@
-public class Person implements Comparable<Person>
-{
+public class Person implements Comparable<Person> {
 
     private String id, fname, lname, sex, occupation, contactNum;
     private int countryCode, areaCode;
 
     public Person(String id, String fname, String lname, String sex, String occupation,
-            String contactNum, int countryCode, int areaCode)
-    {
+            String contactNum, int countryCode, int areaCode) {
         this.id = id;
         this.fname = fname;
         this.lname = lname;
@@ -17,38 +15,31 @@ public class Person implements Comparable<Person>
         this.areaCode = areaCode;
     }
 
-    public String getFName()
-    {
+    public String getFName() {
         return fname;
     }
 
-    public void setFName(String fname)
-    {
+    public void setFName(String fname) {
         this.fname = fname;
     }
 
-    public String getLName()
-    {
+    public String getLName() {
         return lname;
     }
 
-    public void setLName(String lname)
-    {
+    public void setLName(String lname) {
         this.lname = lname;
     }
 
-    public String getFullName()
-    {
+    public String getFullName() {
         return this.getFName() + " " + this.getLName();
     }
 
-    public String getSex()
-    {
+    public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex)
-    {
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
@@ -57,102 +48,94 @@ public class Person implements Comparable<Person>
      * 
      * @return His or Her.
      */
-    public String getPronoun()
-    {
+    public String getPronoun() {
         return this.getSex().equals("Male") ? "His" : "Her";
     }
 
-    public String getOccupation()
-    {
+    public String getOccupation() {
         return occupation;
     }
 
-    public void setOccupation(String occupation)
-    {
+    public void setOccupation(String occupation) {
         this.occupation = occupation;
     }
 
-    public String getContactNum()
-    {
+    public String getContactNum() {
         return contactNum;
     }
 
-    public void setContactNum(String contactNum)
-    {
+    public void setContactNum(String contactNum) {
         this.contactNum = contactNum;
     }
 
-    public int getAreaCode()
-    {
+    public int getAreaCode() {
         return areaCode;
     }
 
-    public String getId()
-    {
+    public String getId() {
         return id;
     }
 
-    public void setId(String id)
-    {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public void setAreaCode(int areaCode)
-    {
+    public void setAreaCode(int areaCode) {
         this.areaCode = areaCode;
     }
 
-    public int getCountryCode()
-    {
+    public int getCountryCode() {
         return countryCode;
     }
 
-    public void setCountryCode(int countryCode)
-    {
+    public void setCountryCode(int countryCode) {
         this.countryCode = countryCode;
     }
 
     /**
-     * @return Full contact number of this person: Country Code + Area Code + Contact Number
+     * @return Full contact number of this person: Country Code + Area Code +
+     *         Contact Number
      */
-    public String getPhoneNumber()
-    {
+    public String getPhoneNumber() {
         return this.countryCode + "-" + this.areaCode + "-" + this.getContactNum();
     }
 
     /**
-     * Compare the lexicographic order of this person object and another person object. <br>
+     * Compare the lexicographic order of this person object and another person
+     * object. <br>
      * <br>
-     * Must compare by last name first, and then first name if both person objects have the same
-     * last name. Assume that no two or more person objects will share the same first name.
+     * Must compare by last name first, and then first name if both person objects
+     * have the same
+     * last name. Assume that no two or more person objects will share the same
+     * first name.
      * 
      * @param o The person object to be compared.
-     * @return Lexicographic flag if this person object should go first before the other person
+     * @return Lexicographic flag if this person object should go first before the
+     *         other person
      *         object. <br>
      *         <br>
      *         -1 if this person object should go first. <br>
      *         <br>
-     *         0 if both person objects share the same last name and first name. <br>
+     *         0 if both person objects share the same last name and first name.
+     *         <br>
      *         <br>
      *         1 if the other person object should go first.
      */
     @Override
-    public int compareTo(Person o)
-    {
+    public int compareTo(Person o) {
         // Complete this method
         // DO NOT REMOVE THE @Override TAG
-        int lastNameComparison = this.lname.compareTo(o.lname);
+        int lastNameComparison = this.lname.compareToIgnoreCase(o.lname);
         if (lastNameComparison != 0) {
             return lastNameComparison;
         }
-        return this.fname.compareTo(o.fname);
+        return this.fname.compareToIgnoreCase(o.fname);
     }
 
     /**
      * Prints the information of this person.
      */
-    public String toString()
-    {
+    public String toString() {
         return String.format("%s is a %s. %s number is %s", this.getFullName(),
                 this.getOccupation(), this.getPronoun(), this.getPhoneNumber());
     }
